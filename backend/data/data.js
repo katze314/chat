ad96cc45c0b18d607b9ac7e31352ab1ea3126f49
katze314 <marsmaennchen@t-online.de>
2025-12-14 00:15:00 +0100

    stuff

---
+++ backend/data/data.js
@@ -0,0 +1,41 @@
+export const chats = [
+    {
+        isGroupChat: false,
+        users: [
+            {
+                name: "John Doe",
+                email: "john@example.com",
+            },
+            {
+                name: "Max Mustermann",
+                email: "max@example.com",
+            }
+        ],
+        _id: "13cda313dc372723b123b123",
+        chatName: "Test",
+    },
+
+    {
+        isGroupChat: false,
+        users: [
+            {
+                name: "John Doe",
+                email: "john@example.com",
+            },
+            {
+                name: "Maxi Mustermann",
+                email: "maxi@example.com",
+            }
+        ],
+        _id: "13cda313dc372723b123b129",
+        chatName: "Testi",
+    },
+
+
+
+
+
+
+];
+
+module.exports={ chats };
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {chats}=require("./data/data")
+
+const app = express();
+
+
+app.get('/', (req,res)=>{
+    res.send("API is Running");
+})
+
+app.get('/api/chat', (req,res) => {
+    res.send(chats);
+})
+
+app.listen(7000, console.log("Server startet on PORT 7000"));
+
