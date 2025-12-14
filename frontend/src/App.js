21598cb76ef4a985a34692ef625d97fd09f271de
katze314 <marsmaennchen@t-online.de>
2025-12-14 12:34:13 +0100

    stuff

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { Button as ChakraButton } from "@chakra-ui/react"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Button colorScheme="blue">Click me</Button>        
+    </div>
+  );
+}
+
+export default App;
