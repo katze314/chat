414f8360b729fab03cb4c724876d6f2f45b78491
katze314 <marsmaennchen@t-online.de>
2025-12-14 22:40:41 +0100

    home frontend

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,10 +1,38 @@
 import '../App.css';
 import { div } from 'framer-motion/client';
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
 //import { Button, HStack } from "@chakra-ui/react"
 
 
 const Homepage = () => {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [cpassword, setCpassword] = useState();
+
+    const submitSignup = () => {};
+
+    const submitLogin = () => {};
+
+    useEffect(() =>{
+        document.getElementById("defaultOpen")?.click();
+    }, []);
+    
+    const openTab = (evt, tabName) => {
+        const tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) tabcontent[i].style.display = "none";
+
+    const tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++)
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+    };
+
+
+
+    
     return (
     <>
     <div class="container">
@@ -17,18 +45,57 @@ const Homepage = () => {
     </div>
     
     <div class="box">
-        
+        <div>
+        <div class="tab">
+            <button id="defaultOpen" class="tablinks" onClick={(e) => openTab(e, 'Login')}>Login</button>
+            <button class="tablinks" onClick={(e) => openTab(e, 'SignUp')}>Sign Up</button>
+        </div>
+
+        <div id="Login" class="tabcontent">
+        <h3>Login</h3>
         <form>
-        <label for="username">username: </label>
-        <input type="text" name="username"></input>    
-        <label for="password">password: </label>
-        <input type="text" name="password"></input>    
-                
+            <label for="username" >Username: </label>
+            <input onChange={(e) => setName(e.target.value)} 
+            type="text" name="username" placeholder="Enter Username"></input>    
+            <br></br>
+            <label for="password">Password:   </label>
+            <input onChange={(e) => setName(e.target.value)} 
+            type="text" name="password" placeholder="Enter Password"></input>    
+            <br></br>
+            <button type="submit" onClick={submitLogin}>Login</button>
         </form>
+        </div>
+
+        <div id="SignUp" class="tabcontent">
+        <h3>Sign Up</h3>
+        <form>
+            <label for="username">Username: </label>
+            <input type="text" name="username" placeholder="Enter Username"
+            onchange={(e) => setName(e.target.value)}           
+            ></input>    
+            <br></br>
+            <label for="password">Password: </label>
+            <input onchange={(e) => setPassword(e.target.value)}  
+            type="text" name="password" placeholder="Enter Password"></input>    
+            <br></br>
+            <label for="cpassword">Confirm Password: </label>
+            <input 
+            onchange={(e) => setCpassword(e.target.value)}  
+            type="text" name="cpassword" placeholder="Confirm Password"></input>    
+            <br></br>
+            <button type="submit" onClick={submitSignup}>Sign Up</button>
+
+        </form>
+        </div>
+
+    </div>
+        
 
     </div>
     </div>
     
+
+
 </>
 );
 
